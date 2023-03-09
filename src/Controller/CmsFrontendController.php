e8b455deb4341a08bce24daa76c3b21269000431
kurz <bastian.kurz@brickfox.de>
2023-03-09 08:03:36 +0100

    [FEATURE] CMS frontend
    - Add, mechanic to load cms pages by given route
    - Add, unit test
    - Fix, phpcs & phpstan

---
+++ src/Controller/CmsFrontendController.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Core\CustomEntityRepositoryInterface;
+use App\Entity\ContentEntity;
+use iterable;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class CmsFrontendController extends AbstractController
+{
+    private CustomEntityRepositoryInterface $customEntityRepository;
+
+    private Environment $twig;
+
+    public function __construct(CustomEntityRepositoryInterface $customEntityRepository, Environment $twig)
+    {
+        $this->customEntityRepository = $customEntityRepository;
+        $this->twig = $twig;
+    }
+
+    #[Route('/{isoCode}/{title}', 'cms.frontend', defaults: ['auth_required' => false], methods: ['GET'])]
+    public function cmsFrontend(string $isoCode, string $title): Response
+    {
+        $entities = $this->customEntityRepository->read(['isoCode' => $isoCode, 'title' => $title], null);
+        if (!is_countable($entities) ||count($entities) === 0) {
+            return new Response($this->twig->render('Frontend/404_not_found.html.twig'), Response::HTTP_NOT_FOUND);
+        }
+
+        /** @var ContentEntity $entity */
+        $entity = $entities[0];
+
+        $content = $this->twig->render('Frontend/cms.html.twig', [
+            'cms' => [
+                'title' => $entity->getTitle(),
+                'text' => $entity->getText()
+            ]
+        ]);
+
+        return new Response($content);
+    }
+}
