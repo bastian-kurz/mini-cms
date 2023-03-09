f2fae48a06026d2eb46cc3cee48f752184704c45
kurz <bastian.kurz@brickfox.de>
2023-03-09 08:05:26 +0100

    - Fix, pipeline issue

--- src/Controller/CmsFrontendController.php
+++ src/Controller/CmsFrontendController.php
@@ -6,7 +6,6 @@ namespace App\Controller;
 
 use App\Core\CustomEntityRepositoryInterface;
 use App\Entity\ContentEntity;
-use iterable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
